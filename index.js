b635586b19646882a63563f97f15c1f267c8b25f
mouad <mouadzed1@gmail.com>
2025-10-29 21:01:44 +0100

    get the anser

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
-const quistion= ["1 Which planet is known as the “Red Planet”?","2. What has a face and two hands, but no arms or legs?"];
- const ans = [['1','2','3','4'],['8','7','6','5']]
+// const quistion = ["1 Which planet is known as the “Red Planet”?", "2. What has a face and two hands, but no arms or legs?"];
+// const ans = [['1', '2', '3', '4'], ['8', '7', '6', '5']]
+// let n = 0;
 
-document.getElementById("ans-1").addEventListener("click", sendans);
-document.getElementById("ans-1").addEventListener("click", next);
+// console.log(n)
+// document.getElementById("ans-1").addEventListener("click", sendans);
+// document.getElementById("ans-1").addEventListener("click", next);
 
+// function next() {
+//   document.getElementById('Quiz-quition').innerHTML = quistion[n];
+//   n++;
+// }
+
+// function sendans() {
+//   document.getElementById('1').innerHTML = ans[n][0];
+//   document.getElementById('2').innerHTML = ans[n][1];
+//   document.getElementById('3').innerHTML = ans[n][2];
+//   document.getElementById('4').innerHTML = ans[n][3];
+// }
+
+
+
+const quistion = ["1 Which planet is known as the “Red Planet”?", "2. What has a face and two hands, but no arms or legs?"];
+const ans = [['Earth', 'Mars', 'Jupiter'], ['clock', 'car', 'plane', 'log']]
+const currectanwers =['Mars', 'clock'];
 let n = 0;
-function next(){
-document.getElementById('Quiz-quition').innerHTML=    quistion[n];
+let score = 0;
 
+
+
+console.log(n)
+document.getElementById("ans-1").addEventListener("click", sendans);
+
+// let anwerCorrect = document.querySelectorAll('.btn-chois');
+// anwerCorrect.addEventListener("click", checkresult)
+// // console.log(anwerCorrect)
+// function checkresult(){
+//   anwerCorrect.innerText
+// }
+
+
+
+function sendans() {
+   document.getElementById('Quiz-quition').innerHTML = quistion[n];
+
+  document.getElementById('1').innerHTML = ans[n][0];
+  document.getElementById('2').innerHTML = ans[n][1];
+  document.getElementById('3').innerHTML = ans[n][2];
+  document.getElementById('4').innerHTML = ans[n][3];
     n++;
-  
+    console.log(currectanwers[n-1])
+
 }
 
-const chois =document.getElementsByClassName('btn').addEventListener("click");
-console.log(chois[0])
+let nam;
+let button = document.querySelectorAll(".btn-chois");
 
-function sendans(){
-
-  document.getElementById('1').innerHTML=ans[n][0];
-   document.getElementById('2').innerHTML=ans[n][1];
-    document.getElementById('3').innerHTML=ans[n][2];
-     document.getElementById('4').innerHTML=ans[n][3];
-}
-
-
-
+for(let i = 0;i <button.length;i++)
+{
+  button[i].addEventListener('click',function(){
+   nam = this.innerText;
  
+      if (nam == currectanwers[n-1] ) {
+        console.log("true");
+        score+=10;
+      }
+      else{
+         console.log("false");
+         score+0;
+      }
+       
+  // alert(button.innerText + " clicked") 
+  })
+}
+
