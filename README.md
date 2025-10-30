6ba036ffd87b94897810405217cee6dc2fb61894
mouad <mouadzed1@gmail.com>
2025-10-30 16:05:33 +0100

    link de figma and canva

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Utilisant-JavaScript
-Création d'une Application de Quiz Interactive en HTML / Bootstrap et Javascript
+figam :
+ https://www.figma.com/design/P8s0gEqX0kocNHwuTiMLih/Untitled?node-id=0-1&t=OEruZheYzs1f0Mck-1
+canva
+https://www.canva.com/design/DAG3R4tA4Sg/5rEwcJD_VY2BN-B1CXNXzw/edit?utm_content=DAG3R4tA4Sg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// const quistion = ["1 Which planet is known as the “Red Planet”?", "2. What has a face and two hands, but no arms or legs?"];
-// const ans = [['1', '2', '3', '4'], ['8', '7', '6', '5']]
-// let n = 0;
-
-// console.log(n)
-// document.getElementById("ans-1").addEventListener("click", sendans);
-// document.getElementById("ans-1").addEventListener("click", next);
-
-// function next() {
-//   document.getElementById('Quiz-quition').innerHTML = quistion[n];
-//   n++;
-// }
-
-// function sendans() {
-//   document.getElementById('1').innerHTML = ans[n][0];
-//   document.getElementById('2').innerHTML = ans[n][1];
-//   document.getElementById('3').innerHTML = ans[n][2];
-//   document.getElementById('4').innerHTML = ans[n][3];
-// }
 
 
+alert("start");
 
-const quistion = ["1 Which planet is known as the “Red Planet”?", "2. What has a face and two hands, but no arms or legs?"];
+
+const quistion = ["1 Which planet is known as the “Red Planet”?", "2. What has a face and two hands, but no arms or legs?", "3. What has a face and two hands, but no arms or legs?", "4. What has a face and two hands, but no arms or legs?", "5. What has a face and two hands, but no arms or legs?"];
 const ans = [['Earth', 'Mars', 'Jupiter'], ['clock', 'car', 'plane', 'log']]
 const currectanwers =['Mars', 'clock'];
 let n = 0;
@@ -31,16 +14,16 @@ let score = 0;
 console.log(n)
 document.getElementById("ans-1").addEventListener("click", sendans);
 
-// let anwerCorrect = document.querySelectorAll('.btn-chois');
-// anwerCorrect.addEventListener("click", checkresult)
-// // console.log(anwerCorrect)
-// function checkresult(){
-//   anwerCorrect.innerText
-// }
 
 
 
 function sendans() {
+  if(n==2)
+      {
+
+       
+      alert(score);
+      }
    document.getElementById('Quiz-quition').innerHTML = quistion[n];
 
   document.getElementById('1').innerHTML = ans[n][0];
@@ -48,8 +31,7 @@ function sendans() {
   document.getElementById('3').innerHTML = ans[n][2];
   document.getElementById('4').innerHTML = ans[n][3];
     n++;
-    console.log(currectanwers[n-1])
-
+ 
 }
 
 let nam;
@@ -64,9 +46,10 @@ for(let i = 0;i <button.length;i++)
         console.log("true");
         score+=10;
       }
+      
       else{
          console.log("false");
-         score+0;
+         
       }
        
   // alert(button.innerText + " clicked") 
